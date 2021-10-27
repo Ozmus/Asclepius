38f62b3ac3dc1c00ff6a048e7500a516146d2134
Ahmet Ozmus <ahmetozmus@Cihangirs-MacBook-Pro.local>
2021-10-27 10:50:19 +0300

    Discord bot is initialized.

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+client = discord.Client()
+load_dotenv()
+@client.event
+async def on_ready():
+    print("I am ready yo!")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello I am Asclepius.')
+
+client.run(os.getenv('TOKEN'))
