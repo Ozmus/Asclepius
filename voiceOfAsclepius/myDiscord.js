66a64d8189c24d949b4741e7305a7dd1666e53fe
yasemin <ysmndmrts991@gmail.com>
2022-02-07 21:25:04 +0300

    Ses botunun hatalari giderildi

--- voiceOfAsclepius/myDiscord.js
+++ voiceOfAsclepius/myDiscord.js
@@ -2,7 +2,6 @@ const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]} );
 const { token } = require("./config.json")
 const  fs  = require("fs");
-const command = require("./command");
 
 client.on("ready", () => {
     console.log(`Logged in as ${client.user.tag}!`)
@@ -11,14 +10,58 @@ client.on("ready", () => {
 client.on("message", async message => {
 
     if (message.content === ">record") {
-        await command.record(message);
+            const voiceChannelId = message.member.voice.channel.id;
+            const voiceChannel = message.guild.channels.cache.get(voiceChannelId);
+            await voiceChannel.join().then(
+                conn => {
+                    let reciever = conn.receiver;
+                    conn.on('speaking', (user, speaking) => {
+                        if (speaking) {
+                            const audioStream = reciever.createStream(user, {mode: 'pcm'});
+                            const memberDirectory = __dirname+"/"+message.member.id;
+                            if(!fs.existsSync(memberDirectory)) {
+                                fs.mkdirSync(memberDirectory, {
+                                    recursive : true
+                                });
+                            }
+                            let filePath = memberDirectory + "/" + Date.now() + ".pcm";
+                            audioStream.pipe(fs.createWriteStream(filePath));
+                        }
+                    });
+                }
+            ).catch(
+                err => { throw err; }
+            );
     }
 
     if (message.content === ">stopRecord") {
-        await command.stopRecording(message);
+        const { channel: voiceChannel } = message.guild.voiceStates.cache.last();
+        voiceChannel.leave();
+        let filePath = __dirname + "/" + message.member.id;
+        const chunks = fs.readdirSync(filePath);
+        mergePCM(chunks,filePath);
     }
 
 });
 
+var inputStream;
+var fileName = __dirname+ "/records/merge" + Date.now() + ".pcm";
+const outputStream = fs.createWriteStream(fileName);
+
+function mergePCM(chunks,filePath) {
+
+    if (chunks.length == 0) {
+        fs.rmdirSync(filePath, {recursive: true});
+        return;
+    }
+
+    let currentfile = filePath + "/" + chunks.shift();
+    inputStream = fs.createReadStream(currentfile);
+    inputStream.pipe(outputStream, { end: false });
+
+    inputStream.on('end', function() {
+        mergePCM(chunks, filePath);
+    });
+}
 
 client.login(token);
