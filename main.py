38c70910e1f9e8bd58ab3aa21638e0a977ca8144
iremkilinc99 <ikilinc@etu.edu.tr>
2022-02-09 15:39:34 +0300

    some api requests written but not done

--- main.py
+++ main.py
@@ -1,20 +1,44 @@
 import discord
 import os
+from discord import Status
+from discord.ext import commands
 from dotenv import load_dotenv
 
-client = discord.Client()
 load_dotenv()
+GUILD = os.getenv('DISCORD_GUILD')
+GENEL = os.getenv('DISCORD_GENEL')
+intents = discord.Intents.all()
+client = discord.Client(intents=intents)
+
 @client.event
 async def on_ready():
-    print("I am ready yo!")
+    await client.wait_until_ready()
+    await client.get_channel(GENEL).send(f'Sahibiniz geldi 👑')
+
+    for guild in client.guilds:
+        if guild.name == "TOBB ETU SEVDALILARI":
+            for member in guild.members:
+                if member.status == discord.Status.online and member != client.user:
+                    print(member, member.status)
+                    await member.create_dm()
+                    await member.dm_channel.send(f'Merhaba bebegim {member.name} imparatorluğuma hoşgeldin.')
+
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
-        return
+    if message.content.startswith('$i'):
+        await message.channel.send("Sahibim geldi :)")
+
+    if message.content.startswith('$y'):
+        await message.channel.send("Sahibimin kankisi geldi :) <3")
 
     if message.content.startswith('$hello'):
-        await message.channel.send('Hello I am Asclepius.')
+        await message.channel.send('Привет I am Asclepius.')
+
+
+
+#@client.event
+#async def on_private_channel_update(before, after):
 
 client.run(os.getenv('TOKEN'))
 
